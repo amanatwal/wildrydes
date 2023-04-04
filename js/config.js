3015021e04dc56586a8151c288b5e5118fdab4a1
amanatwal <113372303+amanatwal@users.noreply.github.com>
2023-04-04 17:06:40 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://roj7k3enma.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://tsvq7qcj0m.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
