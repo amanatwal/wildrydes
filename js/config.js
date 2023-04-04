22f7ecf9f00e823a50b558350ef9992de65ee52d
amanatwal <113372303+amanatwal@users.noreply.github.com>
2023-04-04 14:55:20 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_vDt4JRvP8", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3f0371icc5a6icd5rb3400a02j", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_lsznnnUBs", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "347bje1ni5b6cjrndp9q9a7hnc", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
