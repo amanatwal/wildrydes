d0e8f653d2ee9ae11e9d70136e4002a871f1b329
amanatwal <113372303+amanatwal@users.noreply.github.com>
2023-04-04 23:15:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_lsznnnUBs", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "347bje1ni5b6cjrndp9q9a7hnc", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_yoMXHymGa", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "4er7i0liudbkfo6m7d8rj2a5pk", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://tsvq7qcj0m.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
